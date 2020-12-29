318996420823f76fefdebba505c044d735c8fba6
Risa Takata <beat1422@icloud.com>
2020-12-29 15:25:35 -0800

    ADD: add 3page-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Friend from "./pages/Friend"
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/About" component={About}/>
+          <Route exact path="/Friend" component={Friend}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Friend.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Friend = () => {
+  return (
+    <div>
+      <h2>Friends</h2>
+      <p>ここにフレンズのリストを書きます</p>
+    </div>
+  )
+}
+
+export default Friend
