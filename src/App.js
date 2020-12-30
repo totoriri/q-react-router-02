ef57d3bb2c2ee61fe702c0a55b500010dc575ae4
Risa Takata <beat1422@icloud.com>
2020-12-30 12:43:38 -0800

    ADD: add the 3 page component and simple routings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
+// import Home from "./pages/Home"
+// import About from "./pages/About"
+// import Friends from "./pages/Friends"
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/friends">Friends</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/friends" component={Friends}/>
+        {/* <Route exact path="/" component={}/>
+        <Route exact path="/" component={}/> */}
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p></p>
+    </div>
+  )
+}
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p></p>
+    </div>
+  )
+}
+
+export const Friends = () => {
+  return (
+    <div>
+      <h1>Friend Page</h1>
+      <p></p>
+    </div>
+  )
+}
